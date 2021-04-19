959f3d41e335714d695a42aef9b2d29a5107c2b3
Pluchart Timothee <tim.pluchart@gmail.com>
2021-04-19 18:57:15 +0200

    ajout fonctionnalité login et signin'

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/User');
+
+mongoose.connect('mongodb+srv://tpluchart:zyxXso9dDCJ8eg3e@cluster0.rdnbl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+useUnifiedTopology: true })
+.then(() => console.log('connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !'});
-});
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.use(bodyParser.json());
+
+  app.use('/api/auth', userRoutes);
 
 module.exports = app;
 
