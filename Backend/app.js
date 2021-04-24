c300093f09cb8f6b94fd5f22effd2fa307bc3d4a
Pluchart Timothee <tim.pluchart@gmail.com>
2021-04-24 18:00:40 +0200

    ajout cors & rate-limit

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,24 @@
+require('dotenv').config() 
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit'); 
+
 
 const app = express();
+
+app.use(rateLimit({         
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}));
+
+app.use(helmet());
+
+app.use(cors({origin: 'http://localhost:4200'}));
 
 const userRoutes = require('./routes/User');
 const sauceRoutes = require('./routes/sauce');
@@ -25,6 +40,11 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.use((req, res, next) => {
+    res.setHeader("X-XSS-Protection", "1; mode=block");
     next();
   });
 
